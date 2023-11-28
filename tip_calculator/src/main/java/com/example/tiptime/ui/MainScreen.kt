57dccb0301785a87fafa89183ec5e1ca3dfcab62
artNehay <artemynehay@gmail.com>
2023-11-28 13:12:21 +0300

    Enable vertical scroll on the main screen

--- tip_calculator/src/main/java/com/example/tiptime/ui/MainScreen.kt
+++ tip_calculator/src/main/java/com/example/tiptime/ui/MainScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -31,7 +33,8 @@ fun MainScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(horizontal = 40.dp),
+            .padding(horizontal = 40.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
