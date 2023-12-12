9ef2327fbecbef8b0d97800c98029195c53c3098
artNehay <artemynehay@gmail.com>
2023-12-12 21:41:11 +0300

    Add navigation buttons to the main screen

--- artspace/src/main/java/com/example/android/artspace/ui/MainScreen.kt
+++ artspace/src/main/java/com/example/android/artspace/ui/MainScreen.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -81,6 +83,28 @@ fun MainScreen(
                         style = MaterialTheme.typography.bodySmall,
                     )
                 }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            NavigationButton(onClick = {}) {
+                Text("Previous")
+            }
+
+            NavigationButton(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End),
+            ) {
+                Text("Next")
             }
         }
     }
--- artspace/src/main/java/com/example/android/artspace/ui/NavigationButton.kt
+++ artspace/src/main/java/com/example/android/artspace/ui/NavigationButton.kt
@@ -1,7 +1,7 @@
 package com.example.android.artspace.ui
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -20,11 +20,10 @@ fun NavigationButton(
 ) {
     Button(
         onClick = onClick,
-        modifier = modifier,
+        modifier = modifier.width(136.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
         ),
-        contentPadding = PaddingValues(horizontal = 52.dp),
         content = content,
     )
 }
