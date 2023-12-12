d8ed2066a0c679bdcbcc476b1ed9d0cbfa3655a3
artNehay <artemynehay@gmail.com>
2023-12-12 20:20:56 +0300

    Add a caption to the painting

--- artspace/src/main/java/com/example/android/artspace/ui/MainScreen.kt
+++ artspace/src/main/java/com/example/android/artspace/ui/MainScreen.kt
@@ -3,13 +3,25 @@ package com.example.android.artspace.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android.artspace.R
@@ -20,20 +32,52 @@ fun MainScreen(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = 20.dp),
+        verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Surface(
             modifier = Modifier
-                .padding(horizontal = 20.dp),
+                .weight(1f)
+                .fillMaxHeight()
+                .wrapContentHeight(Alignment.CenterVertically),
             shadowElevation = 16.dp,
         ) {
             Image(
-                painter = painterResource(R.drawable.the_lover_letter_fragonard),
+                painter = painterResource(R.drawable.the_love_letter_fragonard),
                 contentDescription = null,
                 modifier = Modifier.padding(36.dp),
             )
+        }
+
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.secondaryContainer,
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.love_letter),
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+                Row {
+                    Text(
+                        text = stringResource(R.string.fragonard),
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.bodySmall,
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = stringResource(R.string.love_letter_date),
+                        fontWeight = FontWeight.Light,
+                        style = MaterialTheme.typography.bodySmall,
+                    )
+                }
+            }
         }
     }
 }
--- artspace/src/main/java/com/example/android/artspace/ui/theme/Type.kt
+++ artspace/src/main/java/com/example/android/artspace/ui/theme/Type.kt
@@ -2,16 +2,15 @@ package com.example.android.artspace.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+    ),
+    bodySmall = TextStyle(
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+    ),
 )
