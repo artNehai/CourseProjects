7f227e838c252c8456f9d681973ee4b04a709ee8
artNehay <artemynehay@gmail.com>
2023-12-22 19:52:16 +0300

    Move the navigation buttons to the bottom bar

--- artspace/src/main/java/com/example/android/artspace/ui/ArtSpaceApp.kt
+++ artspace/src/main/java/com/example/android/artspace/ui/ArtSpaceApp.kt
@@ -1,12 +1,19 @@
 package com.example.android.artspace.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.android.artspace.ArtSpaceViewModel
 import com.example.android.artspace.ui.theme.ArtSpaceTheme
@@ -17,7 +24,31 @@ fun ArtSpaceApp(
     modifier: Modifier = Modifier,
     viewModel: ArtSpaceViewModel = viewModel(),
 ) {
-    Scaffold { innerPadding ->
+    Scaffold(
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .padding(
+                        horizontal = 8.dp,
+                        vertical = 16.dp,
+                    ),
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                NavigationButton(onClick = { viewModel.previousPainting() }) {
+                    Text("Previous")
+                }
+
+                NavigationButton(
+                    onClick = { viewModel.nextPainting() },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.End),
+                ) {
+                    Text("Next")
+                }
+            }
+        }
+    ) { innerPadding ->
         MainScreen(
             modifier = modifier
                 .consumeWindowInsets(innerPadding)
--- artspace/src/main/java/com/example/android/artspace/ui/MainScreen.kt
+++ artspace/src/main/java/com/example/android/artspace/ui/MainScreen.kt
@@ -3,15 +3,11 @@ package com.example.android.artspace.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -86,28 +82,6 @@ fun MainScreen(
                     },
                     style = MaterialTheme.typography.bodySmall,
                 )
-            }
-        }
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Row(
-            modifier = Modifier
-                .padding(horizontal = 8.dp)
-                .padding(bottom = 16.dp),
-            horizontalArrangement = Arrangement.Start,
-        ) {
-            NavigationButton(onClick = { viewModel.previousPainting() }) {
-                Text("Previous")
-            }
-
-            NavigationButton(
-                onClick = { viewModel.nextPainting() },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentWidth(Alignment.End),
-            ) {
-                Text("Next")
             }
         }
     }
