6a2fe0dedc9383c675974bb64fa32c4ced527889
artNehay <artemynehay@gmail.com>
2023-12-25 16:14:27 +0300

    Make navigation bar distinctive color

--- artspace/src/main/java/com/example/android/artspace/ui/theme/Theme.kt
+++ artspace/src/main/java/com/example/android/artspace/ui/theme/Theme.kt
@@ -5,9 +5,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
     primaryContainer = light_primary_container,
@@ -24,7 +24,9 @@ fun ArtSpaceTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = true
         }
     }
 
