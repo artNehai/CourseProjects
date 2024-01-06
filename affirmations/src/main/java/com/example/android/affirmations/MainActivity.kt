8440e7c38210ddbce4494e9a47c3dc6f984c39ea
artNehay <artemynehay@gmail.com>
2024-01-06 23:13:43 +0300

    Implement a lazy list of affirmation cards

--- affirmations/src/main/java/com/example/android/affirmations/MainActivity.kt
+++ affirmations/src/main/java/com/example/android/affirmations/MainActivity.kt
@@ -3,14 +3,32 @@ package com.example.android.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.android.affirmations.data.Datasource
+import com.example.android.affirmations.model.Affirmation
 import com.example.android.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val datasource = Datasource()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +38,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AffirmationsApp()
+                    AffirmationsApp(
+                        listOfAffirmations = datasource.loadAffirmations()
+                    )
                 }
             }
         }
@@ -28,5 +48,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AffirmationsApp() {
+fun AffirmationsApp(
+    modifier: Modifier = Modifier,
+    listOfAffirmations: List<Affirmation> = Datasource().loadAffirmations(),
+) {
+    LazyColumn(modifier) {
+        items(listOfAffirmations) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp),
+            )
+        }
+    }
+}
+
+@Composable
+fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Modifier = Modifier,
+) {
+    Card(modifier) {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop,
+            )
+
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun AffirmationsAppPreview() {
+    AffirmationsTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AffirmationsApp()
+        }
+    }
 }
