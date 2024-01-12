9cb3d37bd64b51939db8d1e965c77fc52e1698c6
artNehay <artemynehay@gmail.com>
2024-01-12 18:03:02 +0300

    Configure the custom app theme

--- superheroes/src/main/java/com/example/android/superheroes/ui/theme/Theme.kt
+++ superheroes/src/main/java/com/example/android/superheroes/ui/theme/Theme.kt
@@ -15,9 +15,70 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val LightColorScheme = lightColorScheme()
+private val LightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    inversePrimary = md_theme_light_secondary,
+    secondary = md_theme_light_onSecondary,
+    onSecondary = md_theme_light_secondaryContainer,
+    secondaryContainer = md_theme_light_onSecondaryContainer,
+    onSecondaryContainer = md_theme_light_tertiary,
+    tertiary = md_theme_light_onTertiary,
+    onTertiary = md_theme_light_tertiaryContainer,
+    tertiaryContainer = md_theme_light_onTertiaryContainer,
+    onTertiaryContainer = md_theme_light_error,
+    background = md_theme_light_errorContainer,
+    onBackground = md_theme_light_onError,
+    surface = md_theme_light_onErrorContainer,
+    onSurface = md_theme_light_background,
+    surfaceVariant = md_theme_light_onBackground,
+    onSurfaceVariant = md_theme_light_surface,
+    surfaceTint = md_theme_light_onSurface,
+    inverseSurface = md_theme_light_surfaceVariant,
+    inverseOnSurface = md_theme_light_onSurfaceVariant,
+    error = md_theme_light_outline,
+    onError = md_theme_light_inverseOnSurface,
+    errorContainer = md_theme_light_inverseSurface,
+    onErrorContainer = md_theme_light_inversePrimary,
+    outline = md_theme_light_surfaceTint,
+    outlineVariant = md_theme_light_outlineVariant,
+    scrim = md_theme_light_scrim,
 
-private val DarkColorScheme = darkColorScheme()
+    )
+
+private val DarkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    inversePrimary = md_theme_dark_secondary,
+    secondary = md_theme_dark_onSecondary,
+    onSecondary = md_theme_dark_secondaryContainer,
+    secondaryContainer = md_theme_dark_onSecondaryContainer,
+    onSecondaryContainer = md_theme_dark_tertiary,
+    tertiary = md_theme_dark_onTertiary,
+    onTertiary = md_theme_dark_tertiaryContainer,
+    tertiaryContainer = md_theme_dark_onTertiaryContainer,
+    onTertiaryContainer = md_theme_dark_error,
+    background = md_theme_dark_errorContainer,
+    onBackground = md_theme_dark_onError,
+    surface = md_theme_dark_onErrorContainer,
+    onSurface = md_theme_dark_background,
+    surfaceVariant = md_theme_dark_onBackground,
+    onSurfaceVariant = md_theme_dark_surface,
+    surfaceTint = md_theme_dark_onSurface,
+    inverseSurface = md_theme_dark_surfaceVariant,
+    inverseOnSurface = md_theme_dark_onSurfaceVariant,
+    error = md_theme_dark_outline,
+    onError = md_theme_dark_inverseOnSurface,
+    errorContainer = md_theme_dark_inverseSurface,
+    onErrorContainer = md_theme_dark_inversePrimary,
+    outline = md_theme_dark_surfaceTint,
+    outlineVariant = md_theme_dark_outlineVariant,
+    scrim = md_theme_dark_scrim,
+)
 
 @Composable
 fun CourseProjectsTheme(
@@ -45,6 +106,7 @@ fun CourseProjectsTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
+        shapes = Shapes,
         typography = Typography,
         content = content
     )
--- superheroes/src/main/java/com/example/android/superheroes/ui/theme/Type.kt
+++ superheroes/src/main/java/com/example/android/superheroes/ui/theme/Type.kt
@@ -1,5 +1,39 @@
 package com.example.android.superheroes.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.android.superheroes.R
 
-val Typography = Typography()
+val Cabin = FontFamily(
+    Font(R.font.cabin, FontWeight.Normal),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    )
+)
