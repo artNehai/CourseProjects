39f4158702ef3ba06ce012bfde584419332f7523
artNehay <artemynehay@gmail.com>
2024-01-13 11:48:04 +0300

    Implement the top app bar

--- superheroes/src/main/java/com/example/android/superheroes/MainActivity.kt
+++ superheroes/src/main/java/com/example/android/superheroes/MainActivity.kt
@@ -3,16 +3,24 @@ package com.example.android.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.android.superheroes.data.HeroesRepository
+import com.example.android.superheroes.model.Hero
 import com.example.android.superheroes.ui.HeroesList
+import com.example.android.superheroes.ui.SuperheroesTopBar
 import com.example.android.superheroes.ui.theme.SuperheroesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             SuperheroesAppTheme {
@@ -20,9 +28,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HeroesList(heroes = HeroesRepository.heroes)
+                    SuperheroesApp(heroes = HeroesRepository.heroes)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun SuperheroesApp(
+    heroes: List<Hero>,
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { SuperheroesTopBar() },
+    ) { paddingValues ->
+        HeroesList(
+            heroes = heroes,
+            modifier = Modifier.padding(paddingValues)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SuperheroesAppPreview() {
+    SuperheroesAppTheme {
+        SuperheroesApp(heroes = HeroesRepository.heroes)
     }
 }
